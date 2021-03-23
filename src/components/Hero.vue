edf40673d0144ef6ae2111efb73b85a0fe099525
Courtney Ring <courtney.ring@imagination.com>
2021-03-23 15:30:32 -0400

    intial commit

---
+++ src/components/Hero.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class='hero'> 
+        <div class='hero__title'>Oyster Bay</div>
+        <div class='hero__subtitle'>Resident Dashboard</div>
+        <div class='hero__cta'>Learn More</div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Hero',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .hero {
+        align-items: flex-start;
+        background-image: url('~@/assets/images/hero.png');
+        background-size: cover;
+        display: flex;
+        flex-direction: column;
+        height: 90vh;
+        justify-content: center;
+        width: 100%;
+    }
+
+    .hero__title {
+        color: white;
+        font-size: 50px;
+    }
+
+    .hero__subtitle {
+        font-size: 30px;
+        color: white;
+    }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+      <Hero />
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Hero from '@/components/Hero.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Hero
+  }
+}
+</script>
